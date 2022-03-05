dc99f0e6d77cbc6cc7d51efca21716b5b19f8279
wangjixin <seabedfish.x@gmail.com>
2022-03-05 22:59:38 +0800

    modify: 隐藏登录api

--- src/api/user.js
+++ src/api/user.js
@@ -1,18 +1,33 @@
 import request from '@/utils/request'
 
 export function login(data) {
-  return request({
-    url: '/user/login',
-    method: 'post',
-    data
+  // return request({
+  //   url: '/user/login',
+  //   method: 'post',
+  //   data
+  // })
+  return new Promise((resolve, reject) => {
+    resolve({
+      data: {
+        token: 'test123'
+      }
+    })
   })
 }
 
 export function getInfo(token) {
-  return request({
-    url: '/user/info',
-    method: 'get',
-    params: { token }
+  // return request({
+  //   url: '/user/info',
+  //   method: 'get',
+  //   params: { token }
+  // })
+  return new Promise((resolve, reject) => {
+    resolve({
+      data: {
+        name: 'admin',
+        avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif'
+      }
+    })
   })
 }
 
