2a155a56d53554d4cff2eb9e60617cb6633c5149
wangjixin <seabedfish.x@foxmail.com>
2023-04-11 15:30:54 +0800

    modify: ai请求时btn显示加载状态

--- src/views/openAi/index.vue
+++ src/views/openAi/index.vue
@@ -5,13 +5,12 @@
     <div class="content">
       <el-input v-model="prompt" class="promptInput" type="textarea" :rows="5" placeholder="输入描述" />
       <div class="repeat-btn">
-        <el-button type="primary" @click="handleAIRepeat">AI回答</el-button>
+        <el-button :loading="loading" type="primary" @click="handleAIRepeat">AI回答</el-button>
       </div>
       <el-input
         v-model="repeat"
         class="repeatInput"
         type="textarea"
-        :rows="5"
         readonly
         placeholder="请耐心等待回答 Ai生成它很快，但是由于网络问题我们需要等待，通常内容越长等待越久 如果长时间没反应请刷新页面重试"
       />
@@ -25,6 +24,7 @@ import { getAiData } from '@/api/openAi'
 export default {
   data() {
     return {
+      loading: false,
       prompt: '',
       repeat: ''
     }
@@ -34,19 +34,23 @@ export default {
       this.repeat = ''
       if (!this.prompt) return
 
+      this.loading = true
       getAiData({ prompt: this.prompt }).then(({ data }) => {
+        this.loading = false
         const message = data && data.aiData || ''
         let index = 0
 
         const interval = setInterval(() => {
-          this.repeat += message[index];
-          index++;
+          this.repeat += message[index]
+          index++
 
           // 当打印完成时，清除定时器
           if (index >= message.length) {
-            clearInterval(interval);
+            clearInterval(interval)
           }
-        }, 150);
+        }, 50)
+      }).catch(() => {
+        this.loading = false
       })
     }
   }
@@ -73,6 +77,8 @@ export default {
     .repeatInput {
       background-color: #f4f4f4;
       color: #333;
+
+      textarea { height: calc(100vh - 350px); }
     }
   }
 }
